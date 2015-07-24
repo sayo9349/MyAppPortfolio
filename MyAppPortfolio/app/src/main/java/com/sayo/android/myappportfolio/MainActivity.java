31990f3302bd6ef1961d7754b5da595ca28ac066
Simon <simon.ayoub@gmail.com>
2015-07-24 21:37:06 +1000

    added button push listeners

--- MyAppPortfolio/app/src/main/java/com/sayo/android/myappportfolio/MainActivity.java
+++ MyAppPortfolio/app/src/main/java/com/sayo/android/myappportfolio/MainActivity.java
@@ -1,9 +1,11 @@
 package com.sayo.android.myappportfolio;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -14,25 +16,19 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void openExternalApp(View view) {
+        Button pressedButtons = (Button)view;
+        String buttonText = pressedButtons.getText().toString();
+
+        displayToast(buttonText);
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+    private void displayToast(String toastString) {
+        Context context = getApplicationContext();
+        CharSequence text = toastString.subSequence(0,toastString.length());
+        int duration = Toast.LENGTH_SHORT;
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
